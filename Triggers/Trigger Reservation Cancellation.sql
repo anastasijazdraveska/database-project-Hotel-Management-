be0af7b11608069cf2c008d6fda908e46dc0ccf2
Anastasija Zdraveska <108356060+anastasijazdraveska@users.noreply.github.com>
2026-09-02 11:08:50 +0200

    Update Trigger Reservation Cancellation.sql

--- Triggers/Trigger Reservation Cancellation.sql
+++ Triggers/Trigger Reservation Cancellation.sql
@@ -1,17 +1,14 @@
-USE HotelManagement1;
+USE HotelManagement;
 GO
-
 CREATE TRIGGER trigger_Reservation_Cancellation
 ON Reservation
 AFTER UPDATE
 AS
 BEGIN
     SET NOCOUNT ON;
-
     UPDATE r
     SET r.CancellationDate = CAST(GETDATE() AS DATE)
     FROM Reservation r
-
     JOIN inserted i
         ON r.ReservationID = i.ReservationID
 
@@ -19,7 +16,6 @@ BEGIN
         ON i.ReservationID = d.ReservationID
 
     WHERE i.ReservationStatus = 'Cancelled'
-      AND d.ReservationStatus <> 'Confirmed';
-
+    AND d.ReservationStatus <> 'Cancelled';
 END;
 GO
