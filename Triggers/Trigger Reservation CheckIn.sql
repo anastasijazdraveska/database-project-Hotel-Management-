15cd2be77a0cc81fb4b814078e8f81ace24a2bcb
Anastasija Zdraveska <108356060+anastasijazdraveska@users.noreply.github.com>
2026-09-02 11:14:52 +0200

    Update Trigger Reservation CheckIn.sql

--- Triggers/Trigger Reservation CheckIn.sql
+++ Triggers/Trigger Reservation CheckIn.sql
@@ -1,6 +1,5 @@
-USE HotelManagement1;
+USE HotelManagement;
 GO
-
 CREATE TRIGGER trigger_Reservation_CheckIn
 ON Reservation
 AFTER UPDATE
@@ -8,37 +7,23 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
+    DECLARE @EmployeeID INT;
+
+    SELECT TOP 1 @EmployeeID = i.EmployeeID
+    FROM inserted i
+    JOIN deleted d ON i.ReservationID = d.ReservationID
+    WHERE i.ReservationStatus = 'Checked In'
+      AND d.ReservationStatus = 'Confirmed';
+
+    EXEC sp_set_session_context @key = N'CurrentEmployeeID', @value = @EmployeeID;
+
     UPDATE r
     SET r.RoomStatusID = rs.RoomStatusID
     FROM Room r
-    JOIN inserted i
-        ON r.RoomID = i.RoomID
-    JOIN deleted d
-        ON i.ReservationID = d.ReservationID
-    JOIN RoomStatus rs
-        ON rs.StatusName = 'Occupied'
+    JOIN inserted i ON r.RoomID = i.RoomID
+    JOIN deleted d ON i.ReservationID = d.ReservationID
+    JOIN RoomStatus rs ON rs.StatusName = 'Occupied'
     WHERE i.ReservationStatus = 'Checked In'
       AND d.ReservationStatus = 'Confirmed';
-
-    INSERT INTO RoomStatusHistory
-    (
-        RoomID,
-        EmployeeID,
-        RoomStatusID,
-        ChangeDateTime
-    )
-    SELECT
-        i.RoomID,
-        i.EmployeeID,
-        rs.RoomStatusID,
-        GETDATE()
-    FROM inserted i
-    JOIN deleted d
-        ON i.ReservationID = d.ReservationID
-    JOIN RoomStatus rs
-        ON rs.StatusName = 'Occupied'
-    WHERE i.ReservationStatus = 'Checked In'
-      AND d.ReservationStatus = 'Confirmed';
-
 END;
 GO
